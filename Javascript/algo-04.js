96920b4a793fd35db84137fb87f0eae74ba6623c
safwan <safwan7342@gmail.com>
2024-03-07 11:08:55 +0100

    first commit

---
+++ Javascript/algo-04.js
@@ -0,0 +1,10 @@
+nb = prompt ("Entrer un nombre");
+
+while(nb < 10 || nb > 20 ){
+    if(nb <10 ){
+        alert("Plus grand")
+    }else{
+    alert("Plus petit")
+    }
+    nb = prompt ("Entrer un nombre");
+    }
