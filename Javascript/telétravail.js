2a9e5f458adc80fb79f6bf56f927246479e76cb2
safwan <safwan7342@gmail.com>
2024-03-08 16:55:02 +0100

    others exercise

--- Javascript/telétravail.js
+++ Javascript/telétravail.js
@@ -5,6 +5,15 @@ let y = parseInt(prompt("choisie un entier"));
 alert ("le somme est " + (x+y));
 alert ("le produit est " + (x*y));
 
-// Exercise 1
+// Exercise 2
 
-let u (Entre)
+let l = parseInt(prompt("choisie un rayon"));
+let t = parseInt(prompt("choisie un rayon"));
+alert ("le périmètre est " + (l+t));
+alert ("le surface est " + (l*t));
+
+// Exercise 3
+
+let U = ("Entrez la valeur de U");
+let I = ("Entrez la valeur de I");
+alert(" La valeur de R est " + (U/I));
