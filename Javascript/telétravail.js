f02e8c9f194fb9c166186efa7aabc82dd178e7ef
safwan <safwan7342@gmail.com>
2024-03-11 15:23:43 +0100

    adding exercise

--- Javascript/telétravail.js
+++ Javascript/telétravail.js
@@ -1,19 +1,48 @@
 // Exercise 1
 
-let x = parseInt(prompt("choisie un entier"));
-let y = parseInt(prompt("choisie un entier"));
-alert ("le somme est " + (x+y));
-alert ("le produit est " + (x*y));
+// let x = parseInt(prompt("choisie un entier"));
+// let y = parseInt(prompt("choisie un entier"));
+// alert ("le somme est " + (x+y));
+// alert ("le produit est " + (x*y));
 
 // Exercise 2
 
-let l = parseInt(prompt("choisie un rayon"));
-let t = parseInt(prompt("choisie un rayon"));
-alert ("le périmètre est " + (l+t));
-alert ("le surface est " + (l*t));
+// let l = parseInt(prompt("choisie un rayon"));
+// let t = parseInt(prompt("choisie un rayon"));
+// alert ("le périmètre est " + (l+t));
+// alert ("le surface est " + (l*t));
 
 // Exercise 3
 
-let U = ("Entrez la valeur de U");
-let I = ("Entrez la valeur de I");
-alert(" La valeur de R est " + (U/I));
+// let x = parseInt(prompt("Entrer la valeur de x"));
+// let y = parseInt(prompt("Entrer la valeur de y"));
+
+// document.write("valeurs de départ : " + x + " et " + y + "<br>");
+
+// x = x + y;
+// y = x - y;
+// x = x - y;
+
+// document.write("valeurs après traitement : " + x + " et " + y + "<br>");
+
+
+// Exercise 4
+
+let a = parseInt(prompt("entrer la valeur de a"));
+let b = parseInt(prompt("entrer la valeur de b"));
+let c = parseInt(prompt("entrer la valeur de c"));
+
+let delta 
+delta = ((b*b)-4*a*c);
+if(delta < 0){
+    alert("pas de solution dans R");
+
+}else{
+    if(delta == 0){
+        alert("x = " + (-b/(2*a)));
+    }else{
+        alert(" x1 = " + (-b - Math.sqrt(delta))/(2*a));
+        alert(" x2 = " + (-b + Math.sqrt(delta))/(2*a));
+    }
+}
+
