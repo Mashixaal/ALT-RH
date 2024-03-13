dc2a95dcd0fd148ff90c8ab16e8afaa1ce7037b3
safwan <safwan7342@gmail.com>
2024-03-13 11:51:06 +0100

    adding code

--- Javascript/tabTpExo1.js
+++ Javascript/tabTpExo1.js
@@ -1,24 +1,25 @@
 let t = [];
 let n = parseInt (prompt("donnez le nombre de'élement du tableau"));
-let min ;
+let min;
 
-for (let i= 0; i < n; i++ ){
+for (let i= 0; i < n; i++){
     // t[i]= parseInt(prompt("entrer la valeur de la case"+(i)));
     t[i]= Math.floor(Math.random()*30);
-};
+}
 // afficher le tableau
-for (let i= 0; i < n; i++ ){
+for (let i= 0; i < n; i++){
     // t[i]= parseInt(prompt("entrer la valeur de la case"+(i)));
     document.write(t[i] + "<br>");
 }
 min = t[0];
+let indice = 0;
 let j = 1;
 
-while (j< t.length){
-    if (t[j]<min){
-    min=t[j]};
-
+while (j < t.length){
+    if (t[j] <= min){
+        min = t[j];
+        indice = j;
+    }
     j++;
-
 } 
-alert ("afficher "+ min);
+alert ("Le min est : "+ min + " son indice est : " + indice);
