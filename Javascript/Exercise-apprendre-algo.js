a4ed36e4060c34faf535eb0dfd98e218743f0d2f
safwan <safwan7342@gmail.com>
2024-03-15 16:15:17 +0100

    adding exo 3

--- Javascript/Exercise-apprendre-algo.js
+++ Javascript/Exercise-apprendre-algo.js
@@ -4,4 +4,10 @@ let prenom;
 prenom = prompt("saisir votre prenom");
 alert("bienvenu " + prenom);
 
+// Exo 2
+
+let i = parseInt(prompt("choisir un nombre"));
+let x = parseInt(prompt("choisir un nombre"));
+alert("le réssultat est " + (i + x));
+
 
