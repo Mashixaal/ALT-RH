c891a4da492e48c16c57ed92662028bbc37d275e
safwan <safwan7342@gmail.com>
2024-03-18 13:32:16 +0100

    adding exo 5

--- Javascript/Exercise-apprendre-algo.js
+++ Javascript/Exercise-apprendre-algo.js
@@ -24,16 +24,54 @@
 
 // Exo 4
 
-let a = parseInt(prompt("Entrer un entier"));
-let b = parseInt(prompt("Entrer un entier"));
-let c = parseInt(prompt("Entrer un entier"));
-let max = a;
+// let a = parseInt(prompt("Entrer un entier"));
+// let b = parseInt(prompt("Entrer un entier"));
+// let c = parseInt(prompt("Entrer un entier"));
+// let max = a;
 
-if(b > max){
-    max = b;
-}
-if(c > max){
-    max = c;
-}
+// if(b > max){
+//     max = b;
+// }
+// if(c > max){
+//     max = c;
+// }
 
-document.write(a +";"+ b +";" +c +" le max est "+ max)
+// document.write(a +";"+ b +";" +c +" le max est "+ max)
+
+// Exo 5
+
+let t=[];
+for (let i = 0; i < 10; i++) {
+    t[i]= Math.floor(Math.random()*20);
+}
+for (let i = 0; i < 10; i++) {
+    document.write("<h1>" + t[i] + " </h1>") ;
+}
+document.write("<h1>")
+for (let i = 0; i < t.length; i++) {
+let x = t[i];
+if((x <= 4)){
+    document.write(" 0-4 --> Catastrophique, il faut tout revoir<br>")
+}else{
+if((x <= 10)){
+    document.write(" 5-10 --> Insuffisant<br>")
+}else{
+if((x <= 14)){
+    document.write(" 11-14 --> Peut mieux faire<br>")
+}else{
+if((x <= 17)){
+    document.write(" 15-17 --> Bien<br>")
+}else{
+if((x <= 20)){
+    document.write(" 18-20 --> Excellent, Bon travail<br>")
+}else{
+    if((x < 0) || (x > 20)){
+        document.write("Saisir une note entre 0 et 20<br>")
+    }
+}
+}
+}
+}
+}
+}
+document.write("</h1>")
