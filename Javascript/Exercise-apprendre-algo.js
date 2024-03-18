bb5c531147cffac8b9c27a926307953ffbfb1a2c
safwan <safwan7342@gmail.com>
2024-03-18 15:03:34 +0100

    adding exo 8

--- Javascript/Exercise-apprendre-algo.js
+++ Javascript/Exercise-apprendre-algo.js
@@ -40,38 +40,55 @@
 
 // Exo 5
 
-let t=[];
-for (let i = 0; i < 10; i++) {
-    t[i]= Math.floor(Math.random()*20);
-}
-for (let i = 0; i < 10; i++) {
-    document.write("<h1>" + t[i] + " </h1>") ;
-}
-document.write("<h1>")
-for (let i = 0; i < t.length; i++) {
-let x = t[i];
-if((x <= 4)){
-    document.write(" 0-4 --> Catastrophique, il faut tout revoir<br>")
-}else{
-if((x <= 10)){
-    document.write(" 5-10 --> Insuffisant<br>")
-}else{
-if((x <= 14)){
-    document.write(" 11-14 --> Peut mieux faire<br>")
-}else{
-if((x <= 17)){
-    document.write(" 15-17 --> Bien<br>")
-}else{
-if((x <= 20)){
-    document.write(" 18-20 --> Excellent, Bon travail<br>")
-}else{
-    if((x < 0) || (x > 20)){
-        document.write("Saisir une note entre 0 et 20<br>")
+// let t=[];
+// for (let i = 0; i < 10; i++) {
+//     t[i]= Math.floor(Math.random()*20);
+// }
+// for (let i = 0; i < 10; i++) {
+//     document.write("<h1>" + t[i] + " </h1>") ;
+// }
+// document.write("<h1>")
+// for (let i = 0; i < t.length; i++) {
+// let x = t[i];
+// if((x <= 4)){
+//     document.write(" 0-4 --> Catastrophique, il faut tout revoir<br>")
+// }else{
+// if((x <= 10)){
+//     document.write(" 5-10 --> Insuffisant<br>")
+// }else{
+// if((x <= 14)){
+//     document.write(" 11-14 --> Peut mieux faire<br>")
+// }else{
+// if((x <= 17)){
+//     document.write(" 15-17 --> Bien<br>")
+// }else{
+// if((x <= 20)){
+//     document.write(" 18-20 --> Excellent, Bon travail<br>")
+// }else{
+//     if((x < 0) || (x > 20)){
+//         document.write("Saisir une note entre 0 et 20<br>")
+//     }
+// }
+// }
+// }
+// }
+// }
+// }
+// document.write("</h1>")
+
+// Exo 8
+
+let n = parseInt(prompt("Entrez un nombre :"));
+
+if (!isNaN(n) && n > 0) {
+    for (let i = 1; i <= n; i++) {
+        let etoiles = '';
+        for (let j = 0; j < i; j++) {
+            etoiles += '*';
+        }
+        document.write(etoiles);
+        document.write("<br/>");
     }
+} else {
+    alert("Entrer un nombre valide et positif.");
 }
-}
-}
-}
-}
-}
-document.write("</h1>")
