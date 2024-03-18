424e4318587f8a8fd4489a23264086122c029fb8
safwan <safwan7342@gmail.com>
2024-03-18 13:30:52 +0100

    adding exo 4

--- Javascript/Exercise-apprendre-algo.js
+++ Javascript/Exercise-apprendre-algo.js
@@ -11,17 +11,29 @@
 // alert("le réssultat est : " + (i + x));
 
 // Exo 3
-let i = parseFloat(prompt("saisir le prix de fabrication"));
-let x = parseFloat(prompt("saisir le prix de vente"));
+// let i = parseFloat(prompt("saisir le prix de fabrication"));
+// let x = parseFloat(prompt("saisir le prix de vente"));
 
-if(i > x){
-    alert("perte de fabrication est " + (i - x));
-} else{
-    if(i < x){
-        alert("profit de vente est " + (x - i));
-    }
-} 
+// if(i > x){
+//     alert("perte de fabrication est " + (i - x));
+// } else{
+//     if(i < x){
+//         alert("profit de vente est " + (x - i));
+//     }
+// } 
 
 // Exo 4
 
+let a = parseInt(prompt("Entrer un entier"));
+let b = parseInt(prompt("Entrer un entier"));
+let c = parseInt(prompt("Entrer un entier"));
+let max = a;
 
+if(b > max){
+    max = b;
+}
+if(c > max){
+    max = c;
+}
+
+document.write(a +";"+ b +";" +c +" le max est "+ max)
