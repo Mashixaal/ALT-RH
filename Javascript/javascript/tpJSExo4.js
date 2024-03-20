6f50541200b167f8244dc2082e277d2733206b59
safwan <safwan7342@gmail.com>
2024-03-20 16:28:47 +0100

    update

--- Javascript/javascript/tpJSExo4.js
+++ Javascript/javascript/tpJSExo4.js
@@ -9,3 +9,13 @@ function annulerStyle(){
     bd[0].classList.remove('active');
     document.getElementById('btnChanger').style.borderRadius = "0%"
 }
+
+function remplacerImage(){
+    let im = document.getElementById('tof');
+    im.setAttribute('src', 'img/goju.png');
+}
+
+function revenirImage(){
+    let im = document.getElementById('tof');
+    im.setAttribute('src' , 'img/zoro.jpeg');
+}
