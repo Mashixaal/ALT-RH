61c7d482a96151a488dc50fd6983437015286f50
safwan <safwan7342@gmail.com>
2024-03-20 16:06:39 +0100

    update

--- Javascript/javascript/tpJSExo4.js
+++ Javascript/javascript/tpJSExo4.js
@@ -1,1 +1,11 @@
-alert("Bonjour");
+function changer(){
+    let bd = document.getElementsByTagName('body');
+    bd[0].classList.add('active');
+    document.getElementById('btnChanger').classList.add('btnChanger');
+}
+
+function annulerStyle(){
+    let bd = document.getElementsByTagName('body');
+    bd[0].classList.remove('active');
+    document.getElementById('btnChanger').style.borderRadius = "0%"
+}
