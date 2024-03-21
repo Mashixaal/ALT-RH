623207472980993becd6f6888dac3944dd1db701
safwan <safwan7342@gmail.com>
2024-03-21 10:12:54 +0100

    first commit

---
+++ Javascript/js/tpJSExo5.js
@@ -0,0 +1,14 @@
+let i = 1; // naruto
+
+function permeter(){
+    let x = document.getElementById("img");
+    if (i == 1){
+        x.setAttribute("src", "img/naruto.png");
+        i = 2;
+    } else {
+        x.setAttribute("src", "img/zoro.jpeg");
+        i = 1; // zoro
+    }
+}
+
+setInterval("permeter()", 2000);
