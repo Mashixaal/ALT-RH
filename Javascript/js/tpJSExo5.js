2eb421458cccefba1e7876a47d67939ea04030ac
safwan <safwan7342@gmail.com>
2024-03-21 14:12:54 +0100

    adding code

--- Javascript/js/tpJSExo5.js
+++ Javascript/js/tpJSExo5.js
@@ -1,32 +1,52 @@
-let i = 1; // naruto
+// let i = 1; // naruto
 
-function permeter(){
-    let x = document.getElementById("img");
-    if (i == 1){
-        x.setAttribute("src", "img/naruto.png");
-        i = 2;
-    } else {
-        x.setAttribute("src", "img/zoro.jpeg");
-        i = 1; // zoro
+// function permeter(){
+//     let x = document.getElementById("img");
+//     if (i == 1){
+//         x.setAttribute("src", "img/naruto.png");
+//         i = 2;
+//     } else {
+//         x.setAttribute("src", "img/zoro.jpeg");
+//         i = 1; // zoro
+//     }
+// }
+
+// let y = setInterval("permeter()", 2000);
+
+// function arreter(){
+//    clearInterval(y)
+//    y = null;
+// }
+
+// function reprendre(){
+//     let x = document.getElementById("img");
+//     if (i == 1){
+//         x.setAttribute("src", "img/naruto.png");
+//         i = 2;
+//     } else {
+//         x.setAttribute("src", "img/zoro.jpeg");
+//         i = 1; // zoro
+//     }
+// }
+
+// Diaporama 
+
+let t = ["goju.png","naruto.png","zoro.jpeg"];
+let j = 0;
+let y = document.getElementById('diapo');
+function suivant() {
+    j++;
+    if(j == t.length){
+        j = 0;
     }
+    y.src = "img/" + t[j]; // y.setAttribute("src","img/"+t[j]);
 }
-
-let y = setInterval("permeter()", 2000);
-
-function arreter(){
-   clearInterval(y)
-   y = null;
+function precedent(){
+    j--;
+    if (j < 0){
+        j = t.length - 1;
 }
-
-function reprendre(){
-    let x = document.getElementById("img");
-    if (i == 1){
-        x.setAttribute("src", "img/naruto.png");
-        i = 2;
-    } else {
-        x.setAttribute("src", "img/zoro.jpeg");
-        i = 1; // zoro
-    }
+    y.src = "img/" + t[j]; // y.setAttribute("src","img/"+t[j]);
 }
 
 
