2a980cd940b7139d61db3ac38bfa11ce89c71bfc
safwan <safwan7342@gmail.com>
2024-03-21 14:38:57 +0100

    updating the code

--- Javascript/js/tpJSExo5.js
+++ Javascript/js/tpJSExo5.js
@@ -1,56 +1,51 @@
-// let i = 1; // naruto
+let i = 1; // naruto
 
-// function permeter(){
-//     let x = document.getElementById("img");
-//     if (i == 1){
-//         x.setAttribute("src", "img/naruto.png");
-//         i = 2;
-//     } else {
-//         x.setAttribute("src", "img/zoro.jpeg");
-//         i = 1; // zoro
-//     }
-// }
+function permeter(){
+    let x = document.getElementById("img");
+    if (i == 1){
+        x.setAttribute("src", "img/naruto.png");
+        i = 2;
+    } else {
+        x.setAttribute("src", "img/zoro.jpeg");
+        i = 1; // zoro
+    }
+}
 
-// let y = setInterval("permeter()", 2000);
+let y = setInterval("permeter()", 2000);
 
-// function arreter(){
-//    clearInterval(y)
-//    y = null;
-// }
+function arreter(){
+   clearInterval(y)
+   y = null;
+}
 
-// function reprendre(){
-//     let x = document.getElementById("img");
-//     if (i == 1){
-//         x.setAttribute("src", "img/naruto.png");
-//         i = 2;
-//     } else {
-//         x.setAttribute("src", "img/zoro.jpeg");
-//         i = 1; // zoro
-//     }
-// }
+function reprendre(){
+    let x = document.getElementById("img");
+    if (i == 1){
+        x.setAttribute("src", "img/naruto.png");
+        i = 2;
+    } else {
+        x.setAttribute("src", "img/zoro.jpeg");
+        i = 1; // zoro
+    }
+}
 
 // Diaporama 
 
 let t = ["goju.png","naruto.png","zoro.jpeg"];
 let j = 0;
-let y = document.getElementById('diapo');
+let n = document.getElementById('diapo');
 function suivant() {
     j++;
     if(j == t.length){
         j = 0;
     }
-    y.src = "img/" + t[j]; // y.setAttribute("src","img/"+t[j]);
+    n.src = "img/" + t[j]; // y.setAttribute("src","img/"+t[j]);
 }
 function precedent(){
     j--;
     if (j < 0){
         j = t.length - 1;
 }
-    y.src = "img/" + t[j]; // y.setAttribute("src","img/"+t[j]);
+    n.src = "img/" + t[j]; // y.setAttribute("src","img/"+t[j]);
 }
-
-
-
-
-
 
