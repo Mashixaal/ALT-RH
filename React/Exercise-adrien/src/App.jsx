180ade772f7e71d3735b5c588be05828a6fb1098
safwan <safwan7342@gmail.com>
2024-04-11 10:01:52 +0200

    update

--- React/Exercise-adrien/src/App.jsx
+++ React/Exercise-adrien/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import Clock from './components/Clock'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const UserList = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=>{
+       fetch('https://jsonplaceholder.typicode.com/users')
+         .then( response => response.json())
+         .then( data => setUsers(data))
+         .catch(error => console.error('Error ', error)) 
+    },[])
 
   return (
-    <>
-    <Clock/>
-    </>
+    <div>
+        <h1> Liste Utilisateur : </h1>
+        {users.map(user=>(
+            <p key={user.id}>{user.name}</p>
+        ))}
+    </div>
   )
 }
 
-export default App
+export default UserList
