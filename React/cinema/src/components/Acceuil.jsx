9acd2913971da56fe92f2b914069a1a0afd0bc08
safwan <safwan7342@gmail.com>
2024-04-22 16:42:14 +0200

    adding films

--- React/cinema/src/components/Acceuil.jsx
+++ React/cinema/src/components/Acceuil.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Card from "./Card";
 
+let API_key ="api_key=4814bdf6f5a651f9666651d6ac18636d";
+let base_url = "https://api.themoviedb.org/3"
 const Acceuil = () => {
   return (
     <>
@@ -12,22 +14,23 @@ const Acceuil = () => {
             </li>
           </ul>
         </nav>
-        <form>
+        <form className="d-flex mx-auto py-1">
           <div className="search-btn">
             <input type="text" placeholder="Entrer le nom de movie" className="inputText" />
             <button><i class="fa-solid fa-magnifying-glass"></i></button>
           </div>
         </form>
+        <h5><a href="#">Favoris</a></h5>
       </div>
       <div className="container">
           <Card/>
+          {/* <Card/>
           <Card/>
           <Card/>
           <Card/>
           <Card/>
           <Card/>
-          <Card/>
-          <Card/>
+          <Card/> */}
       </div>
     </>
   );
--- React/cinema/src/components/Card.jsx
+++ React/cinema/src/components/Card.jsx
@@ -1,20 +1,252 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (movie) => {
+  console.log(movie.info);
+
   return (
     <>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/ic5Hs6zYcM4YUuxyezFUAFQie39.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>The Spiderwick Chronicles</h4>
+                    <p className='rating'>88%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Adaptée des romans éponymes, elle raconte les aventures de la famille Grace (les jumeaux, Jared et Simon, leur sœur, Mallory, et leur mère, Helen) quand elle emménage dans la maison ancestrale, à l’abandon, et le mystère qui plane sur l’arrière-arrière-oncle. Celui-ci avait découvert l’existence d’un monde parallèle, alternativement féerique… et un peu inquiétant.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/7O4iVfOMQmdCSxhOg1WnzG1AgYT.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Shōgun</h4>
+                    <p className='rating'>87%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    En l'an 1600 au Japon, le seigneur Yoshii Toranaga lutte pour sa survie alors que ses ennemis du Conseil des régents se liguent contre lui, lorsqu'un mystérieux navire européen est retrouvé échoué dans un village de pêcheurs voisin.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/9Ycz7yYRf9V4jk3YXwcZhFtbNcF.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>X-Men</h4>
+                    <p className='rating'>87%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Un groupe de mutants s'efforce de protéger un monde qui les hait et les craint.</div>
+            </div>
+        </div>
         <div className='movie'>
             <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/AnsSKR9LuK0T9bAOcPVA3PUvyWj.jpg" className='poster' />
             <div className='movie-details'>
                 <div className='box'>
                     <h4 className='title'>Fallout</h4>
-                    <p className='rating'>9.7</p>
+                    <p className='rating'>84%</p>
                 </div>
                 <div className='overview'>
                     <h1>aperçu</h1>
-                    Everyone knows the stories of Robin Hood and Maid Marian, but this the story of what happens next. Out of the shadows, a legend returns to save her people from the tyranny of the disgraced Sheriff of Nottingham. Robin Hood remains at war and Marian must put her own combat skills to the test, creating a new tale that will be heralded throughout the ages.</div>
+                    Tirée de l'un des plus grands jeux vidéo de tous les temps, Fallout est une histoire d'inégalités dans un monde où tous sont égaux face au chaos. Deux cents ans après l'apocalypse, les pacifiques habitants de luxueux abris antiatomiques sont contraints de retourner dans le monde incroyablement complexe, joyeusement étrange et extrêmement violent qui les attend à la surface.</div>
             </div>
         </div>
+        <div className='movie'>
+            <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/yJm61MmTMjOmNXxPxdoaIkdqnOm.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Harry Potter </h4>
+                    <p className='rating'>78%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Le pouvoir de Voldemort s’étend. Celui‐ci contrôle maintenant le Ministère de la Magie et Poudlard. Harry, Ron et Hermione décident de terminer le travail commencé par Dumbledore, et de retrouver les derniers Horcruxes pour vaincre le Seigneur des Ténèbres. Mais il reste bien peu d’espoir aux trois sorciers, qui doivent réussir à tout prix.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/pQYHouPsDw32FhDLr7E3jmw0WTk.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Exhuma</h4>
+                    <p className='rating'>78%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Une famille aisée vivant à Los Angeles fait appel à deux jeunes chamans pour sauver leur nouveau-né après avoir été victime d'une série d'événements paranormaux.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/8EqXMy7g5HkKBYlV8ItlctQaz6S.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Parasyte</h4>
+                    <p className='rating'>77%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Quand des parasites inconnus s'emparent violemment d'hôtes humains et gagnent du pouvoir, l'humanité se mobilise pour lutter contre cette menace qui ne cesse de croître.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/pZIBf6sc5ZcascB6lKHo5NhXcOz.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>The Beekeeper</h4>
+                    <p className='rating'>75%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    La quête brutale de vengeance d’un homme prend des proportions démesurées alors que son passé d’agent secret d’une puissante organisation clandestine connue sous le nom des Apiculteurs est révélé.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/sh7Rg8Er3tFcN9BpKIPOMvALgZd.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Civil War</h4>
+                    <p className='rating'>75%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Dans un futur proche où les États-Unis sont au bord de l'effondrement et où des journalistes embarqués courent pour raconter la plus grande histoire de leur vie : La fin de l'Amérique telle que nous la connaissons.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/vlsFBK0tdCgH17c50wOjJPZHWT3.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Le Problème à 3 corps</h4>
+                    <p className='rating'>75%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Dans la Chine des années 60, une jeune femme prend une décision dont les répercussions à travers le temps et l'espace vont affecter le monde d'aujourd'hui. Lorsque les lois de la nature se délitent inexplicablement sous les yeux d'un groupe soudé de brillants scientifiques, ils unissent leurs forces à celles d'un inspecteur peu orthodoxe pour affronter la plus grande menace de l'histoire de l'humanité.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/t1SULuKu2HMukgJdtL9HfBHumwa.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Kung Fu Panda 4</h4>
+                    <p className='rating'>72%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Après trois aventures dans lesquelles le guerrier dragon Po a combattu les maîtres du mal les plus redoutables grâce à un courage et des compétences en arts martiaux inégalés, le destin va de nouveau frapper à sa porte pour l'inviter à enfin se reposer. Plus précisément, pour être nommé chef spirituel de la vallée de la Paix.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/3KNaBebKwV8mdmVDjiZEmTqaaD2.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Land of Bad</h4>
+                    <p className='rating'>72%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Lorsqu’une mission spéciale de la Delta Force tourne terriblement mal, un pilote de drone de l’armée de l’air, Reaper ne dispose que de 48 heures pour réussir ce qui est devenu une opération de sauvetage hors de contrôle. Sans armes et sans autre moyen de communication que le drone en vol, la mission au sol se transforme soudainement en un affrontement mortel lorsque l’ennemi découvre l’équipe.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/xAHthaVYtwADKYORPkLzd6MgX0P.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Road House</h4>
+                    <p className='rating'>71%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Dalton, un ancien combattant de l'UFC tente d'échapper à son sombre passé et à son penchant pour la violence. Il est repéré par Frankie, propriétaire d'un relais routier dans les Keys de Floride. Elle l'engage comme nouveau videur dans l'espoir d'empêcher un gang violent, travaillant pour le patron du crime Brandt, de détruire son bar bien-aimé.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/i367eMUXwj9LtNqrVlw1NXGRLx7.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Avatar</h4>
+                    <p className='rating'>71%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    An inside look at one of the most anticipated movie sequels ever with James Cameron and cast.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/11HRdHtqynRb0JpiQJZzVXtyKDb.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Les Aventuriers de Noé</h4>
+                    <p className='rating'>71%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Vini et Tito, deux souris artistes de music-hall, voient leurs rêves de succès tomber à l’eau lorsque la Terre est noyée par le Déluge. Aidés par un coup de pouce du destin, ils réussissent à monter à bord de l’Arche de Noé. Propulsés au beau milieu d’un différend opposant les animaux herbivores et carnivores, Vini et Tito ont la solution parfaite pour apaiser les tensions : un concours de chansons !</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/nKnWr062zhRvk48NtK27zz3oLgS.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Godzilla x Kong</h4>
+                    <p className='rating'>67%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Après leur confrontation explosive, Godzilla et Kong doivent se réunir pour affronter une menace colossale cachée dans notre monde, qui remet en question leur existence même - et la nôtre. Plongez dans l'histoire de ces Titans, leurs origines et les mystères de l'île du Crâne et au-delà, et découvrez la bataille mythique qui a contribué à forger ces êtres extraordinaires et les a liés à l'humanité pour toujours.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/qTXDVZ9UEby3U73jpUA2XIdIh57.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>S.O.S. Fantômes</h4>
+                    <p className='rating'>65%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Lorsque la découverte d'un ancien artefact libère une force maléfique, les Ghostbusters, nouveaux et anciens, doivent unir leurs forces pour protéger leur maison et sauver le monde d'une seconde ère glaciaire.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/gLZMvG1sUHo7HdG3Q24JIbnMsii.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>L'Amour au pied du mur</h4>
+                    <p className='rating'>65%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Elle est une jeune pianiste qui prépare une audition. Lui est un concepteur de jeux qui ne peut travailler que dans le silence le plus complet. Dans leurs appartements mitoyens séparés par un mur épais comme du papier, sauront-ils vivre en harmonie l'un avec l'autre... et plus si affinités ?</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/kHxJHwSGjC0Fjl7ktWeWg7UrVF8.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Le salaire de la peur</h4>
+                    <p className='rating'>58%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Afin d'empêcher une explosion qui ferait des centaines de victimes, une équipe clandestine est formée pour transporter de la nitroglycérine à travers un désert parsemé de dangers.</div>
+            </div>
+        </div>
+        <div className='movie'>
+            <img src="https://media.themoviedb.org/t/p/original/aN1xLZKQCDYrwK7GVEf9b3jpxYL.jpg" className='poster' />
+            <div className='movie-details'>
+                <div className='box'>
+                    <h4 className='title'>Breathe</h4>
+                    <p className='rating'>53%</p>
+                </div>
+                <div className='overview'>
+                    <h1>aperçu</h1>
+                    Maya et sa fille sont obligées de vivre sous terre après que la Terre ait été rendue inhabitable par un manque d'oxygène. Seuls de brefs voyages à la surface sont possibles grâce à une combinaison à oxygène ultramoderne fabriquée par le mari de Maya, Darius, qu'elle présume mort. Lorsqu'un couple mystérieux arrive, prétendant connaître Darius, Maya accepte de les laisser entrer dans leur bunker, mais sont-ils vraiment ce qu'ils semblent être ?</div>
+            </div>
+        </div>
+       
+        
     </>
   )
 }
