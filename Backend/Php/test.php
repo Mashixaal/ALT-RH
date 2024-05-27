08c78bbf45fc4330228abb32f4cb7431d1334223
abuufahad <abuufahad0702@gmail.com>
2024-05-27 16:11:18 +0200

    exercise of  php

---
+++ Backend/Php/test.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+
+	</head>
+	<body>
+		
+    <!-- Exersice 1 -->
+    
+		<?php echo "Asalamou aleikom";
+	var_dump(1 + 2) ; echo '<br/>';
+	var_dump('1'+ '2'); echo '<br/>';
+	var_dump(1 . 2) ; echo '<br/>';
+	var_dump('1'. '2'); echo '<br/>';
+	var_dump("3 fruits" + '1 légume' . 'donnent'); echo '<br/>';
+	var_dump("3 fruits" . '1 légume' + 'donnent'); echo '<br/>';
+	var_dump(1/2 + true - "-2");echo '<br/>';
+	?>
+	
+	</body>
+</html>
