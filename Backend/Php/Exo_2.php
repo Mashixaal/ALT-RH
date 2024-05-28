cfe942b338e9b2a714b618c42795276d8c921d1e
abuufahad <abuufahad0702@gmail.com>
2024-05-28 11:23:52 +0200

    first commit

---
+++ Backend/Php/Exo_2.php
@@ -0,0 +1,21 @@
+<table>
+    <?php for($cpt = 1 ; $cpt <= 10 ; $cpt++) { ?>
+
+        <tr>
+            <td>7</td>
+            <td> x </td>
+            <td><?php echo $cpt ?></td>
+            <td> = </td>
+            <td><?php echo $cpt * 7 ?></td>
+        </tr>
+
+<?php } ?>
+</table>
+
+<ul>
+<?php for($cpt = 1 ; $cpt <= 10 ; $cpt++) { ?>
+
+    <li>7 x <?php echo $cpt ?> = <?php echo $cpt * 7 ?></li>
+
+    <?php } ?>
+</ul>
