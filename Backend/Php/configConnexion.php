d7afaf012d0614d1c78faa61dacef90facf97f41
mashixaal <safwan7342@gmail.com>
2024-05-30 14:46:33 +0200

    changing the letters

--- Backend/Php/configConnexion.php
+++ Backend/Php/configConnexion.php
@@ -2,7 +2,7 @@
 function openConnexion(){
     try {
         $bdd = new PDO('mysql:host=localhost;
-        bdname=exemple;charset=utf8','root','');
+        dbname=exemple;charset=utf8','root','');
         echo "Connexion réussie";
     } catch (Exception $e) {
         die('Erreur vérifie votre chaine de connexion' . $e->getMessage());
