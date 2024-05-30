db67c75ef77679ebdae2fbaebfe2f07d834c0a6b
mashixaal <safwan7342@gmail.com>
2024-05-30 16:37:08 +0200

    first commit

---
+++ Backend/Php/voiture.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="transaction.php" method="get">
+        <label for="immat">Immatriculation</label>
+        <input type="text" name="immat" id="immat">
+        <br>
+        <label for="couleur">Couleur</label>
+        <input type="text" name="couleur" id="couleur">
+        <br>
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
+</html>
