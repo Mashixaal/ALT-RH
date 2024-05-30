b1252ab3ff65278ba035eec328913645d5d3cbab
mashixaal <safwan7342@gmail.com>
2024-05-30 16:38:53 +0200

    execute queries

--- Backend/Php/transaction.php
+++ Backend/Php/transaction.php
@@ -12,7 +12,13 @@ $ch = "CREATE TABLE IF NOT EXISTS voiture(id INT AUTO_INCREMENT,
 // axec pour exécuter des requétes de màj.
 $maCnx->exec($ch);
 
-// Insérer des data
-$ch ="INSERT INTO voiture(immat, couleur
-VALUES ('1001', 'Blue'), ('1002', 'Rouge'), ('1003', ('Jaune');";
-$maCnx->prepare($ch)->execute();
+if (isset($_REQUEST['immat']) && isset($_REQUEST['couleur'])) {
+    $immat = $_REQUEST['immat'];
+    $couleur = $_REQUEST['couleur'];
+    // Insérer des data
+    $ch ="INSERT INTO voiture(immat, couleur)
+    VALUES (?,?);";
+    $maCnx->prepare($ch)->execute([$immat,$couleur]);
+}
+
+
